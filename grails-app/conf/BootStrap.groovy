7e831615c2c501cbde082f741271e83304b9cad8
jholme <jholme@iii.com>
2015-05-08 17:41:01 -0700

    bootstrap image instructions
    Some comments to assist the user in creating bootstrap image data.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,7 +5,28 @@ class BootStrap {
 		environments {
 			development {
 				
+				/*
+				 * This bootstrap relies on having 6 jpeg images (named as shown below with 'Photo.originalFilename' values),
+				 * located on the filesystem at C:\$file.upload.directory\$gallery.path.
+				 * 
+				 * The value of 'file.upload.directory' is set in Config.groovy - the default is C:\\fogalFiles
+				 * 
+				 * JPEG images are expected to have these metadata fields populated:
+				 * EXIF - User Comment (Photo.title)
+				 * EXIF - Image Description (Photo.description)
+				 * EXIF - Date/Time Original (Photo.photoDate)
+				 * IPTC - City (amalgamated as Photo.location)
+				 * IPTC - Sub-location (amalgamated as Photo.location)
+				 * IPTC - Province/State (amalgamated as Photo.location)
+				 * IPTC - Country/Primary Location Name (amalgamated as Photo.location)
+				 * 
+				 * The tool I used to populated image metadata is found at:
+				 * http://freeweb.siol.net/hrastni3/foto/exif/exiftoolgui.htm
+				 * 
+				 */
+				
 				// default gallery and photos (1)
+				// image path for this gallery is C:\fogalFiles\daydead
 				Gallery daydead = new Gallery(name:"Day of the Dead", description:"This gallery is for 'Day of the Dead' photos", path:"daydead")
 				Photo photo1 = new Photo(title:"Photo 1", originalFilename:"photo1.jpg", thumbnailFilename:"photo1_thumbnail.jpg", description:"Photo One", location:"Oaxaca", photoDate:"2015-05-06", fileSize:0)
 				photo1.save(flush:true)
@@ -19,6 +40,7 @@ class BootStrap {
 				daydead.save(flush:true)
 				
 				// default gallery and photos (2)
+				// image path for this gallery is C:\fogalFiles\ostuff
 				Gallery otherStuff = new Gallery(name:"Other Stuff", description:"This gallery is for other stuff", path:"ostuff")
 				Photo photo4 = new Photo(title:"Photo 4", originalFilename:"photo4.jpg", thumbnailFilename:"photo4_thumbnail.jpg", description:"Photo One", location:"Oaxaca", photoDate:"2015-05-06", fileSize:0)
 				photo4.save(flush:true)
